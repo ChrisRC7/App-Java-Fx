14eac7026206dead66eaec7760a8e1d985abdd13
CHRISTOPHER DAVID RODRIGUEZ CORDERO <chrodriguez@estudiantec.cr>
2022-08-12 17:23:12 -0600

    Cuarto commit, ya se puede cargar archivos

--- src/controlador/Cargar.java
+++ src/controlador/Cargar.java
@@ -1,18 +1,24 @@
 package controlador;
 
-import java.io.File;
-
 import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.FileChooser;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.geometry.*;
+import javafx.scene.paint.*;
+import javafx.scene.canvas.*;
+import javafx.scene.text.*;
+import javafx.scene.Group;
+import javafx.scene.shape.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.collections.*;
+import java.io.*;
+import javafx.stage.FileChooser;
 
-public class Cargar extends Application{
-    
+public class Cargar extends Application {
+
     @Override
     public void start(Stage stage) {
 
@@ -25,18 +31,70 @@ public class Cargar extends Application{
             FileChooser escoge_archivo= new FileChooser();
 
             //Se escoge el titulo
-            escoge_archivo.setTitle("Escoge archivo");
+            escoge_archivo.setTitle("Buscar archivo");
+            
+
+            //Se limita al tipo de archivo que se puede selecionar
+            escoge_archivo.getExtensionFilters().addAll(
+                        new FileChooser.ExtensionFilter("CSV", "*.csv*")
+                    );
 
             //Se inicia el archivo
             escoge_archivo.setInitialDirectory(new File("C:\\"));
-            
 
-        }
+            //Se crea un Label
+            Label label1= new Label("Archivo no selecionado");
 
+            //Se crea un boton
+            Button button= new Button("Escoger Archivo");
 
+            //Se crea un evento tipo Handler
+            EventHandler<ActionEvent> evento1=new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent e){
+
+                    //Se toma el archivo
+                    File file= escoge_archivo.showOpenDialog(stage);
+                    
+                        if(file!=null) {
+                            label1.setText(file.getAbsolutePath()+ " selected");
+                        }
+                    
+                }
+            };
+
+            button.setOnAction(evento1);
+
+  
+        // create a VBox
+        VBox vbox = new VBox(30, label1, button);
+  
+        // set Alignment
+        vbox.setAlignment(Pos.CENTER);
+  
+        // create a scene
+        Scene scene = new Scene(vbox, 800, 500);
+  
+        // set the scene
+        stage.setScene(scene);
+  
+        stage.show();
     }
-
-
-
-    
+  
+    catch (Exception e) {
+  
+        System.out.println(e.getMessage());
+    }
 }
+  
+// Main Method
+public static void main(String args[])
+{
+  
+    // launch the application
+    launch(args);
+}
+}
+
+            
+        
+
